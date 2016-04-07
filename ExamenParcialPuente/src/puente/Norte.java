f4be47fe11b30d354458fc4a9b6adb6e92491045
jaimevlz7 <jaime.velez2@correo.icesi.edu.co>
2016-04-07 16:41:09 -0500

    Update Norte.java

--- ExamenParcialPuente/src/puente/Norte.java
+++ ExamenParcialPuente/src/puente/Norte.java
@@ -2,20 +2,27 @@ package puente;
 
 import java.util.concurrent.Semaphore;
 
+//Clase Norte
+
 public class Norte extends Thread {
 
-	
+	//Atributos de la clase: conexión con clase puente. Inicializacion del semaforo para sincronizar.
 	
 	private Puente puente;
 	private Semaphore semaforo;
 	
+	
+	// Constructor de la clase
 	public Norte(Puente p){
 		puente = p;
+		
+	//creo el math.random(); para que el semaforo me permita pasar x cantidad entre 1 y 10
     	int cant = 1 + (int) (Math.random() * 10);
 
 		semaforo = new Semaphore(cant);
 
 	}
+	//metodo run() creacion del hilo y permitir agregar carros cada y tiempo
 	public void run() {
 
         while (true) {
